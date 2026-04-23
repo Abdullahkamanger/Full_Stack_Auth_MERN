be829697898c0955d1ecd143bc1d9bd314c458c0
Abdullahkamanger <abdullahkamanger300@gmail.com>
2026-04-23 12:02:20 +0500

    fix: Update Axios baseURL to use production backend URL

--- Frontend/src/api/ApiInstance.js
+++ Frontend/src/api/ApiInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // 1. Create the Axios Instance (Crucial for Cookies)
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/auth", // Replace with your backend URL
+  baseURL: "https://full-stack-auth-mern-backend.vercel.app/api/auth", // Replace with your backend URL
   withCredentials: true, // This allows the "token" cookie to be sent
 });
 
